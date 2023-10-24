adea85aff2baf79ba93aab89a079f572f6713b88
babayaga-mp4 <tsyrraina@gmail.com>
2023-10-24 19:18:14 +0530

    Streaming implemented successfully

--- Streaming_ChatCompletion.py
+++ Streaming_ChatCompletion.py
@@ -1,18 +1,25 @@
 import grpc
 from concurrent import futures
 import time
-import generated_code_from_proto  # Replace with the actual module name
+import chat_pb2_grpc
+import chat_pb2  # Replace with the actual module name
+from QA_Scratch import get_chunks_from_openai_api
 
-
-class ChatAssistantServicer(generated_code_from_proto.ChatAssistantServicer):
+class ChatAssistantServicer(chat_pb2_grpc.ChatAssistantServicer):
     def GetResponse(self, request, context):
         # This is where you would interface with the OpenAI API
         # You can yield the response in chunks, simulating "typing"
         for chunk in get_chunks_from_openai_api(request.content):
-            yield generated_code_from_proto.AssistantReply(content=chunk)
+            yield chat_pb2.AssistantReply(content=chunk)
             time.sleep(0.05)  # Typing delay
 
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-generated_code_from_proto.add_ChatAssistantServicer_to_server(ChatAssistantServicer(), server)
-server.add_insecure_port('[::]:50051')
+chat_pb2_grpc.add_ChatAssistantServicer_to_server(ChatAssistantServicer(), server)
+server.add_insecure_port('localhost:50051')
 server.start()
+print("Server started. Listening on localhost:50051.")
+try:
+    while True:
+        time.sleep(86400)  # Sleep for a day. The server will run indefinitely until interrupted.
+except KeyboardInterrupt:
+    server.stop(0)
--- Streaming_chat.py
+++ Streaming_chat.py
@@ -1,8 +1,27 @@
 import grpc
-import generated_code_from_proto  # Replace with the actual module name
+import chat_pb2
+import chat_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = generated_code_from_proto.ChatAssistantStub(channel)
-response_iterator = stub.GetResponse(generated_code_from_proto.UserQuery(content="Your user query here"))
-for response in response_iterator:
-    print(response.content)
+def main():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = chat_pb2_grpc.ChatAssistantStub(channel)
+
+    while True:
+        # Display initial prompt
+        print("\nHello! How can I assist you today?")
+
+        # Get user input
+        user_query = input("Enter your query (or 'exit' to quit): ")
+
+        # Exit loop if user types 'exit'
+        if user_query.lower() == 'exit':
+            break
+
+        print("\nAssistant: ", end="", flush=True)
+        response_iterator = stub.GetResponse(chat_pb2.UserQuery(content=user_query))
+        for response in response_iterator:
+            print(response.content, end="", flush=True)
+            sys.stdout.flush()
+
+if __name__ == '__main__':
+    main()
